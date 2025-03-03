99f3e94e0e02eadab0005f4dc994aec6fb2da056
Maria Francisca Mellado <136738816+MelladoDev@users.noreply.github.com>
2025-03-03 06:22:50 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+   preview: {
+    host: true,
+    allowedHosts: ['bookstore-owzt.onrender.com']
+   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
